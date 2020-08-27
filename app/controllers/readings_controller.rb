c94dc5171588367e1b3323ce7b599860533ac8dd
Ernest Khasanzhinov <khasanzhinov@gmail.com>
2020-08-27 21:05:06 +0300

    implement reading controller

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -2,8 +2,26 @@
 
 class ReadingsController < ApplicationController
   def create
+    params.require(:household_token)
+    params.permit(:temperature, :humidity, :battery, :household_token)
+
+    id, number = CalcNumberAndIDService.new(params.permit(:household_token)).call
+
+    reading_value = ReadingValue.new(
+      params.permit(:temperature, :humidity, :battery, :household_token)
+            .merge(id: id)
+            .merge(number: number)
+    )
+
+    PushToReadingCacheService.new(reading_value).call
+    ProcessReadingJob.enque(id)
+
+    render json: ::CreateReadingSerializer.new(reading_value).serialized_json
   end
 
   def show
+    params.require(:id)
+
+    render json: ::ReadingSerializer.new(FindReadingService.new(params.permit(:id)).call).serialized_json
   end
 end
