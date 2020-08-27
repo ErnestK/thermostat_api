4064a11922a625d70a98f1daa4a25d4c322235b6
Ernest Khasanzhinov <ernest.khasanzhinov@campstar.com>
2020-08-27 19:46:56 +0300

    add factories

---
+++ spec/factories/thermostat.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :thermostat do
+    household_token { "1" }
+    location { "baker street 1, apt 3" }
+    created_at       { Time.now }
+    updated_at       { Time.now }
+  end
+end
