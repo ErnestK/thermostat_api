1dd56d1e0f391b1c5ac12af4e8e2fcda7bbe31bc
Ernest Khasanzhinov <ernest.khasanzhinov@campstar.com>
2020-08-27 18:15:52 +0300

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+thermostat_1 = Thermostat.create(household_token: '1', location: 'baker street 1, apt 1')
+Thermostat.create(household_token: '2', location: 'baker street 1, apt 2')
+Thermostat.create(household_token: '3', location: 'baker street 1, apt 3')
+
+AddReadingService.new(
+  id: 1,
+  number: 1,
+  household_token: thermostat_1.household_token,
+  temperature: '6',
+  humidity: '78',
+  battery_charge: '12'
+).call
