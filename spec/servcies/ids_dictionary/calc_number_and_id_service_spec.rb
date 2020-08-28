550775284f73e07a1262ea0f9cbb9becb5b11a09
Ernest Khasanzhinov <khasanzhinov@gmail.com>
2020-08-28 19:24:46 +0300

    remove comment

--- spec/servcies/ids_dictionary/calc_number_and_id_service_spec.rb
+++ spec/servcies/ids_dictionary/calc_number_and_id_service_spec.rb
@@ -1,36 +1,3 @@
-# # frozen_string_literal: true
-
-# module IdsDictionary
-#   class CalcNumberAndIdService
-#     extend  Dry::Initializer
-#     include Dry::Monads[:result]
-#     include ::WithRedisNaming
-
-#     param :household_token
-
-#     def call
-#       GetMaxReadingId.new.call.bind do |last_id|
-#         get_number_for_household_token.bind do |number|
-#           increment_id_and_number(number, last_id)
-#         end
-#       end
-#     end
-
-#     private
-
-#     def get_number_for_household_token
-#       Redis.current.hget(ids_readings_collection_name, household_token.to_sym).map(&:to_i).first
-#     end
-
-#     def increment_id_and_number(number, last_id)
-#       Redis.current.hmset(ids_readings_collection_name,
-#         household_token.to_sym, number.to_i + 1,
-#         :last_id, last_id.to_i + 1
-#       )
-#     end
-#   end
-# end
-
 # frozen_string_literal: true
 
 require 'rails_helper'
