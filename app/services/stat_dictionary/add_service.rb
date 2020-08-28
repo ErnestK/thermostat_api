86d4d3ec2dc1795dfb386d5da1b7c5c962b3c912
Ernest Khasanzhinov <khasanzhinov@gmail.com>
2020-08-28 19:26:44 +0300

    implement add_service

---
+++ app/services/stat_dictionary/add_service.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module StatDictionary
+  class AddService
+    extend  Dry::Initializer
+    include Dry::Monads[:result]
+    include ::WithRedisNaming
+
+    param :reading_value
+
+    def call
+      GetAllService.new.call.bind do |stat_dictionary_value|
+        AggregateReadingsService.new(reading_value, stat_dictionary_value).call.bind do |agg_stat_dictionary_value|
+          write_to_dict(agg_stat_dictionary_value)
+        end
+      end
+    rescue StandardError => ex
+      Failure("Exception, during added data to stats dictionary: #{ex.message}")
+    end
+
+    private
+
+    def write_to_dict(stat_dictionary_value)
+      Redis.current.hmset(
+        dict_collection_name,
+        :min_temperature,
+        stat_dictionary_value.min_temperature,
+        :max_temperature,
+        stat_dictionary_value.max_temperature,
+        :avg_temperature,
+        stat_dictionary_value.avg_temperature,
+        :min_humidity,
+        stat_dictionary_value.min_humidity,
+        :max_humidity,
+        stat_dictionary_value.max_humidity,
+        :avg_humidity,
+        stat_dictionary_value.avg_humidity,
+        :min_battery_charge,
+        stat_dictionary_value.min_battery_charge,
+        :max_battery_charge,
+        stat_dictionary_value.max_battery_charge,
+        :avg_battery_charge,
+        stat_dictionary_value.avg_battery_charge,
+        :count,
+        stat_dictionary_value.count,
+        :last_id,
+        @reading_value.id
+      )
+
+      Success(true)
+    end
+  end
+end
