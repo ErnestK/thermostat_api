b21363e94deaa2cec59f34ce2e565ec96328b768
Ernest Khasanzhinov <khasanzhinov@gmail.com>
2020-08-28 18:14:12 +0300

    rework add reading service

--- app/services/add_reading_service.rb
+++ app/services/add_reading_service.rb
@@ -4,26 +4,28 @@ class AddReadingService
   extend  Dry::Initializer
   include Dry::Monads[:result]
 
-  param :reading_value
+  param :reading_id
 
   def call
-    ValidateReadingService.new(reading_value).call.bind do
-      RefreshStatDictionaryService.new(reading_value).call.bind do
-        write_reading_db
+    CacheReadings::GetService.new(@reading_id).call.bind do |reading_value_from_cache|
+      ValidateReadingService.new(reading_value_from_cache).call.bind do
+        StatDictionary::AddService.new(reading_value_from_cache).call.bind do
+          write_reading_db(reading_value_from_cache)
+        end
       end
     end
   end
 
   private
 
-  def write_reading_db
+  def write_reading_db(reading_value_from_cache)
     Reading.create(
-      id: @reading_value.id,
-      number: @reading_value.number,
-      thermostat_id: Thermostat.find_by(household_token: @reading_value.household_token).id,
-      temperature: @reading_value.temperature,
-      humidity: @reading_value.humidity,
-      battery_charge: @reading_value.battery_charge
+      id: reading_value_from_cache.id,
+      number: reading_value_from_cache.number,
+      thermostat_id: Thermostat.find_by(household_token: reading_value_from_cache.household_token).id,
+      temperature: reading_value_from_cache.temperature,
+      humidity: reading_value_from_cache.humidity,
+      battery_charge: reading_value_from_cache.battery_charge
     )
 
     Success(true)
