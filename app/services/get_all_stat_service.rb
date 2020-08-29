67ce37e54a983f62ed33a711a5beec24cd43bcc5
Ernest Khasanzhinov <khasanzhinov@gmail.com>
2020-08-29 16:50:30 +0300

    implement get_all_stat_service

---
+++ app/services/get_all_stat_service.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class GetAllStatService
+  extend  Dry::Initializer
+  include Dry::Monads[:result]
+
+  def call
+    StatDictionary::GetAllService.new.call.bind do |stat_dictionary_value|
+      IdsDictionary::GetMaxReadingId.new.call.bind do |max_reading_id|
+        CacheReadings::GetAllService.new(stat_dictionary_value.last_id, max_reading_id).call.bind do |cached_readings|
+          cached_readings.map do |cached_reading|
+            stat_dictionary_value = StatDictionary::AggregateReadingsService.new(cached_reading, stat_dictionary_value).call.value!
+          end
+
+          Success(stat_dictionary_value)
+        end
+      end
+    end
+  end
+end
