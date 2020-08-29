51d65ef4822091c908da6c4c21c4ff1a6f5f2c07
Ernest Khasanzhinov <khasanzhinov@gmail.com>
2020-08-29 13:21:48 +0300

    implement push_service

---
+++ app/services/cache_readings/push_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module CacheReadings
+  class PushService
+    extend  Dry::Initializer
+    include Dry::Monads[:result]
+    include ::WithRedisNaming
+
+    param :reading_value
+
+    def call
+      Success(Redis.current.hset(cache_readings_collection_name, reading_value.id, reading_value.to_h.transform_keys(&:to_s)))
+    end
+  end
+end
