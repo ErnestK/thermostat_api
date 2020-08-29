b8f255e8ea9f8c4ce2c25672ec95f981e504a0b4
Ernest Khasanzhinov <khasanzhinov@gmail.com>
2020-08-29 13:30:19 +0300

    implement get_service

---
+++ app/services/cache_readings/get_service.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module CacheReadings
+  class GetService
+    extend  Dry::Initializer
+    include Dry::Monads[:result]
+    include ::WithRedisNaming
+
+    param :reading_id
+
+    def call
+      str = Redis.current.hget(cache_readings_collection_name, reading_id)
+      return Success(nil) unless str
+
+      hash = JSON.parse(str.gsub(':', '').gsub('=>', ':').gsub(":nil", ":null")).symbolize_keys
+
+      Success(
+        ReadingValue.new(
+          id: hash[:id],
+          number: hash[:number],
+          temperature: hash[:temperature],
+          humidity: hash[:humidity],
+          battery_charge: hash[:battery_charge],
+          household_token: hash[:household_token],
+          updated_at: hash[:updated_at],
+          created_at: hash[:created_at]
+        )
+      )
+    end
+  end
+end
